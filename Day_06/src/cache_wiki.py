50d060b9dded237a5fd1d036dc2df9353f4d31d9
Nail <ahmetgarievnail@gmail.com>
2023-12-23 18:54:16 +0300

    ex00 links parsing ready

---
+++ Day_06/src/cache_wiki.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import requests
+url = "https://en.wikipedia.org/wiki/Six_degrees_of_separation"
+contents = requests.get(url).text
+soup = BeautifulSoup(contents, 'html.parser')
+# print(soup.prettify())
+# toc = soup.find(class_='toc')
+items = soup.find_all(class_='div-col')
+# items = items.find("a")
+it = items[0].find_all("a")
+print(*it, sep="\n")
+hrefs = []
+for item in it:
+    hrefs.append(item.get("href"))
+print(*hrefs, sep="\n")
