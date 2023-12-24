5802028ba66276f39dcf74308ee9776bb5071083
Mitchell Dorren <mitcheld@va-e2.kzn.21-school.ru>
2023-12-24 15:49:24 +0300

    ex00 done 75%

--- Day_06/src/cache_wiki.py
+++ Day_06/src/cache_wiki.py
@@ -1,13 +1,73 @@
-from bs4 import BeautifulSoup
 import requests
-url = "https://en.wikipedia.org/wiki/Six_degrees_of_separation"
-contents = requests.get(url).text
-soup = BeautifulSoup(contents, 'html.parser')
-# print(soup.prettify())  # display whole html page
-items = soup.find_all(class_='div-col')
-it = items[0].find_all("a")
-print(*it, sep="\n")
-hrefs = []
-for item in it:
-    hrefs.append(item.get("href"))
-print(*hrefs, sep="\n")
+from bs4 import BeautifulSoup
+import networkx as nx
+import json
+import matplotlib.pyplot as plt
+from time import sleep
+from random import random
+
+
+def crawl_web(start_url, depth):
+    visited = set()  # Словарь для отслеживания посещенных страниц
+    graph = nx.DiGraph()  # Создание направленного графа
+
+    def crawl(name, url, level):
+        global counter
+        url_begin = 'https://en.wikipedia.org'
+        url = url_begin + url
+        # Проверка, посещали ли мы эту страницу и достигнут ли максимальный уровень
+        if url not in visited and level <= depth:
+            counter += 1
+            print(counter)
+            print(url)
+            visited.add(url)  # Добавляем текущий URL в список посещенных
+
+            # Отправка HTTP-запроса и получение содержимого веб-страницы
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            see_also = soup.find(id='See_also')
+            if (see_also is not None):
+                see_also = see_also.find_next("ul").find_all("li")
+                # hrefs = []
+                for item in see_also:
+                    if counter > 5:
+                        break
+                    # hrefs.append(item.a.get("href"))
+                    graph.add_edge(
+                        name, item.a.get("href"))
+                    crawl(item.a.get("href"), item.a.get("href"), level + 1)
+                    sleep(random())
+
+    # Запуск процесса обхода веба
+    crawl(start_url.split('/')[-1], start_url, 0)
+
+    return graph
+
+
+# Пример использования
+counter = 0
+start_url = '/wiki/Six_degrees_of_separation'  # URL, с которого начинается обход
+depth = 1  # Максимальный уровень обхода
+
+graph = crawl_web(start_url, depth)  # Получение направленного графа
+
+# Сохранение графа в файл JSON
+data = nx.node_link_data(graph)
+with open('graph.json', 'w') as f:
+    json.dump(data, f)
+
+# # Строим граф с помощью matplotlib
+# pos = nx.spring_layout(graph)
+# nx.draw(graph, pos, with_labels=True)
+# nx.draw_networkx_edges(graph, pos, edge_color='gray')
+
+# # Отображаем граф
+# plt.show()
+
+# # Сохраняем граф в формате PNG
+# plt.savefig('graph.png')
+
+nx.draw(graph, with_labels=True)
+plt.savefig('plotgraph.png', dpi=300, bbox_inches='tight')
+plt.show()
